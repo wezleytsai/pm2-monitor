49a036eb0cf49b34cb42d2418fbd7c5fb6f3f73b
Wesley Tsai <wesley.tsai@postmates.com>
2016-02-05 23:12:17 -0800

    remove comment about alias

--- util/next.js
+++ util/next.js
@@ -1,5 +1,4 @@
 // require this to use ES6 in all further required modules
-// options.alias exposes aliases to subsequent modules
 require('babel-register')({
     ignore: function(filename) {
         return /node_modules/.test(filename);
